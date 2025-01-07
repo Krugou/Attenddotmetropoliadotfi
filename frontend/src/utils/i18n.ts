912de127aec5679c8c6d68d643ba41829bee209c
Krugou <59641229+Krugou@users.noreply.github.com>
2025-01-07 16:45:12 +0200

    i18n

---
+++ frontend/src/utils/i18n.ts
@@ -0,0 +1,40 @@
+/**
+ * @module i18n
+ */
+
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
+import enTranslation from '../locales/en/translation.json';
+import fiTranslation from '../locales/fi/translation.json';
+import svTranslation from '../locales/sv/translation.json';
+
+/**
+ * Initialize and configure i18next with translation resources.
+ */
+i18n
+  .use(initReactI18next) // Pass the i18n instance to react-i18next.
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      fi: {
+        translation: fiTranslation,
+      },
+      sv: {
+        translation: svTranslation,
+      },
+    },
+    lng: 'en', // Default language is English.
+    fallbackLng: 'en', // Fallback language is English.
+    interpolation: {
+      escapeValue: false, // Not escape value, so it allows to use HTML in translations.
+    },
+  });
+
+/**
+ * The i18next instance with the application's translation resources.
+ * @type {i18n.i18n}
+ */
+export default i18n;
