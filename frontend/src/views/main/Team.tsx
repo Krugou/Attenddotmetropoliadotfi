da405133c326a950d5e7dc85b6378f19fb8aef14
Krugou <59641229+Krugou@users.noreply.github.com>
2025-01-07 16:17:39 +0200

    Update team section in frontend to include additional development members

--- frontend/src/views/main/Team.tsx
+++ frontend/src/views/main/Team.tsx
@@ -4,7 +4,7 @@ const Team: React.FC = () => {
   return (
     <div className='flex flex-col items-center p-4 m-4 bg-white border-2 border-gray-200 rounded-md shadow-lg justify-evenly h-1/2'>
       <h1 className='mb-4 text-4xl font-bold text-metropoliaMainOrange'>
-        Development Team
+        Original Development Team
       </h1>
       <div className='flex flex-col gap-2 mb-4 text-3xl text-gray-700 md:flex-row font-regular'>
         <div className='p-4 border-2 rounded-md'>
@@ -19,6 +19,16 @@ const Team: React.FC = () => {
       </div>
 
       <p className='text-gray-500'>The initials form the acronym "JAK".</p>
+      <div>
+        <div className='mt-4'>
+          <h2 className='mb-4 text-3xl font-bold text-metropoliaMainOrange'>
+            Additional Development
+          </h2>
+          <div className='p-4 text-3xl text-gray-700 border-2 rounded-md font-regular'>
+            Nestori Laine
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
