972399048674ddc1790d89ac26586d181d412858
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-01-26 12:48:25 +0200

    Refactor NavigationButton to enforce path format and simplify click handling

--- frontend/src/components/main/buttons/NavigationButton.tsx
+++ frontend/src/components/main/buttons/NavigationButton.tsx
@@ -1,25 +1,29 @@
 import React, {FC} from 'react';
 import {useNavigate} from 'react-router-dom';
+
 /**
  * Props for the NavigationButton component.
  */
 interface NavigationButtonProps {
-  path: string;
+  path: `/${string}`; // Ensure path always starts with /
   label: string;
 }
 
 /**
  * A button component that navigates to a specified path when clicked.
- * The button is only rendered if a user is provided.
+ * Always navigates from root (/).
  */
 const NavigationButton: FC<NavigationButtonProps> = ({path, label}) => {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate(path);
+  };
   return (
     <button
       type='button'
       className='px-4 py-2 mx-2 text-white transition border border-white rounded font-heading sm:w-full w-fit bg-metropoliaMainOrange hover:bg-metropoliaSecondaryOrange focus:outline-none focus:ring-2 focus:ring-metropoliaMainOrange'
-      onClick={() => navigate(path)}>
+      onClick={handleClick}>
       {label}
     </button>
   );
