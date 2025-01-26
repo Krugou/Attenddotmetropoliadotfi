7c36ae9151dff384d312ce67c7547e76103e2959
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-01-26 11:26:24 +0200

    Enhance ESLint configuration with TypeScript rules and import ordering

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
   ],
   ignorePatterns: [
     'dist',
@@ -21,9 +23,20 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    '@typescript-eslint/strict-boolean-expressions': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    'import/order': ['error', {'newlines-between': 'always'}],
     'react-refresh/only-export-components': [
       'warn',
       {allowConstantExport: true},
     ],
+  },
+  parserOptions: {
+    project: './tsconfig.json',
   },
 };
