a86b0b5e17c7509dcae39d0a988de7ad08de0cb3
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-04 21:53:21 +0200

    Add central API index file to consolidate API exports

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,20 @@
+import {authApi} from './auth';
+import {courseApi} from './course';
+import {adminApi} from './admin';
+import {secureApi} from './secure';
+import {worklogApi} from './worklog';
+
+export * from './auth';
+export * from './course';
+export * from '../types/auth';
+// export * from '../types/course';
+
+export const apiHooks = {
+  ...authApi,
+  ...courseApi,
+  ...adminApi,
+  ...secureApi,
+  ...worklogApi,
+};
+
+export default apiHooks;
