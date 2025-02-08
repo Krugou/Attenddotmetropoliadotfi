6c4b34d2757a9cf4a407a50e7cc633acd6e3224b
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-08 16:57:26 +0200

    Refactor WelcomeModal to accept storageKey as a prop and improve modal styling

--- frontend/src/components/main/modals/WelcomeModal.tsx
+++ frontend/src/components/main/modals/WelcomeModal.tsx
@@ -1,6 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 
+interface WelcomeModalProps {
+  storageKey: string;
+}
+
 /**
  * WelcomeModal component.
  * This component is responsible for displaying a welcome modal for first-time visitors.
@@ -10,31 +14,35 @@ import {useTranslation} from 'react-i18next';
  * The welcome modal contains a welcome message and a close button.
  * When the close button is clicked, the modal is hidden.
  *
+ * @param {WelcomeModalProps} props - Component props
+ * @param {string} props.storageKey - Key to use for localStorage check
  * @returns {JSX.Element} The rendered WelcomeModal component.
  */
-const WelcomeModal = () => {
+const WelcomeModal: React.FC<WelcomeModalProps> = ({storageKey}) => {
   const [showModal, setShowModal] = useState(false);
   const {t} = useTranslation();
 
   useEffect(() => {
-    const firstVisit = localStorage.getItem('firstVisit');
-    if (!firstVisit) {
+    const hasVisited = localStorage.getItem(storageKey);
+    if (!hasVisited) {
       setShowModal(true);
-      localStorage.setItem('firstVisit', 'no');
+      localStorage.setItem(storageKey, 'true');
     }
-  }, []);
+  }, [storageKey]);
 
   return (
     showModal && (
-      <div className='fixed bottom-0 right-0 z-50 max-w-xs p-6 m-6 bg-white rounded-lg shadow-lg sm:max-w-md md:max-w-lg lg:max-w-xl'>
+      <div className='fixed bottom-2 right-0 z-50 max-w-xs p-6 m-6 bg-white rounded-lg shadow-lg sm:max-w-md md:max-w-lg lg:max-w-xl'>
         <div>
           <h2 className='mb-2 text-2xl font-heading'>{t('welcome.title')}</h2>
           <p className='mb-4'>{t('welcome.description')}</p>
-          <button
-            className='px-4 py-2 text-white bg-blue-500 rounded-sm'
-            onClick={() => setShowModal(false)}>
-            {t('common.close')}
-          </button>
+          <div className='flex justify-end'>
+            <button
+              className='px-4 py-2 text-white font-bold bg-metropolia-main-orange rounded-sm'
+              onClick={() => setShowModal(false)}>
+              {t('common.close')}
+            </button>
+          </div>
         </div>
       </div>
     )
