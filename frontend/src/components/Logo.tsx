9caf12face10d178f74b9bf0bb8a7e60a31be94c
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-10 02:11:36 +0200

    stupid stuff

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/logo.css';
+
 /**
  * Logo component.
  * This component is responsible for rendering the logo of the application.
  * It includes a div with class "logo" that contains the logo design, and a paragraph that contains the name of the application.
  * The logo design is made up of nested divs with various classes to apply the necessary CSS styles.
  * The name of the application is styled with various Tailwind CSS classes.
+ * Clicking on the logo triggers a "crazy eye" animation.
  *
  * @returns {JSX.Element} The rendered Logo component.
  */
 const Logo = () => {
+  const [isCrazy, setIsCrazy] = useState(false);
+
+  const handleClick = () => {
+    setIsCrazy(true);
+    // Reset the crazy state after animation ends
+    setTimeout(() => setIsCrazy(false), 2000);
+  };
+
   return (
     <div>
-      <div className='p-2 m-4 logo'>
+      <div className='p-2 m-4 logo' onClick={handleClick}>
         <div className='oval-shape bg-metropolia-main-orange dark:bg-metropolia-main-orange-dark'>
-          <div className='big-brother-eye'>
-            <div className='eye'></div>
+          <div className={`big-brother-eye ${isCrazy ? 'crazy' : ''}`}>
+            <div className={`eye ${isCrazy ? 'crazy' : ''}`}></div>
           </div>
         </div>
       </div>
