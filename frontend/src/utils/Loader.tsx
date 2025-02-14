423440104012116dfc7ac868996502f50eec5bf4
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-14 08:23:34 +0200

    Enhance Loader component with improved animations and styling

--- frontend/src/utils/Loader.tsx
+++ frontend/src/utils/Loader.tsx
@@ -6,24 +6,33 @@ const Loader = () => {
   const {t} = useTranslation('common');
 
   return (
-    <div className='flex-col gap-4 w-full flex items-center justify-center'>
-      <motion.h1
-        className='text-2xl text-metropolia-main-orange'
-        initial={{opacity: 0, y: -20}}
-        animate={{opacity: 1, y: 0}}
-        transition={{duration: 0.5, ease: 'easeOut'}}>
-        {t('common:metropoliaLoading')}
-      </motion.h1>
-
-      <motion.div
-        animate={{rotate: 360}}
-        transition={{duration: 2, repeat: Infinity, ease: 'linear'}}
-        className='w-20 h-20 border-4 border-transparent text-orange-600 text-4xl flex items-center justify-center border-t-orange-600 rounded-full'>
+    <div className='flex-col gap-4 w-full h-full flex items-center justify-center'>
+      <div className='bg-metropolia-support-white  p-6 rounded-2xl flex flex-col items-center justify-center'>
         <motion.div
-          animate={{rotate: -360}}
-          transition={{duration: 1.5, repeat: Infinity, ease: 'linear'}}
-          className='w-16 h-16 border-4 border-transparent text-blue-600 text-2xl flex items-center justify-center border-t-blue-600 rounded-full'></motion.div>
-      </motion.div>
+          animate={{rotate: 360}}
+          transition={{duration: 1, repeat: Infinity, ease: 'linear'}}
+          className='w-20 h-20 border-4 border-transparent text-metropolia-main-orange text-4xl flex items-center justify-center border-t-metropolia-main-orange rounded-full'>
+          <motion.div
+            animate={{rotate: -360}}
+            transition={{duration: 2, repeat: Infinity, ease: 'linear'}}
+            className='w-16 h-16 border-4 border-transparent text-blue-600 text-2xl flex items-center justify-center border-t-blue-600 rounded-full'></motion.div>
+        </motion.div>
+        <motion.h1
+          className='text-2xl font-bold text-metropolia-main-orange tracking-wider'
+          initial={{opacity: 0, scale: 0.5}}
+          animate={{
+            opacity: [0.5, 1, 0.5],
+            scale: [0.98, 1.02, 0.98],
+            y: [0, -2, 0],
+          }}
+          transition={{
+            duration: 2,
+            repeat: Infinity,
+            ease: 'easeInOut',
+          }}>
+          ✨ {t('common:metropoliaLoading')} ✨
+        </motion.h1>
+      </div>
     </div>
   );
 };
