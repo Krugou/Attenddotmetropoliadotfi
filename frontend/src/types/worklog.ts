7a1974134dbc3d87c4e48f7d18ad397907dcb926
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-14 09:15:28 +0200

    Add WorkLogCourse and ActiveEntry interfaces to enhance work log data structure

--- frontend/src/types/worklog.ts
+++ frontend/src/types/worklog.ts
@@ -9,3 +9,31 @@ export interface WorkLogEntry {
   };
   status: number;
 }
+
+export interface WorkLogCourse {
+  work_log_course_id: number;
+  name: string;
+  code: string;
+  description: string;
+  start_date: Date;
+  end_date: Date;
+}
+
+export interface ActiveEntry {
+  entry_id: number;
+  userid: number;
+  work_log_course_id: number;
+  start_time: Date;
+  end_time: Date;
+  description: string;
+  status: string;
+  course: {
+    work_log_course_id: number;
+    name: string;
+    code: string;
+    description: string;
+    start_date: Date;
+    end_date: Date;
+    required_hours: number;
+  };
+}
