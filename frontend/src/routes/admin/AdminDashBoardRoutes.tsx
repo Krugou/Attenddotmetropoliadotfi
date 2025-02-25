b008e3816038c3017cc120cf11a6c82844341d8f
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-25 17:34:11 +0200

    Remove error logs route from AdminDashboard for streamlined navigation

--- frontend/src/routes/admin/AdminDashBoardRoutes.tsx
+++ frontend/src/routes/admin/AdminDashBoardRoutes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import AdminDashboard from '../../views/main/admin/AdminDashboard';
-import AdminErrorLogs from '../../views/main/admin/Dashboard/AdminErrorLogs';
 import AdminFeedback from '../../views/main/admin/AdminFeedback';
 import AdminGuide from '../../views/main/admin/Dashboard/AdminGuide';
 import AdminLogs from '../../views/main/admin/Dashboard/AdminLogs';
@@ -21,7 +20,6 @@ const AdminDashBoardRoutes = () => {
         <Route path='stats' element={<AdminStats />} />
         <Route path='logs' element={<AdminLogs />} />
         <Route path='user-feedback' element={<AdminFeedback />} />
-        <Route path='errorlogs' element={<AdminErrorLogs />} />
         <Route path='server-status' element={<AdminServerStatus />} />{' '}
       </Route>
     </Routes>
