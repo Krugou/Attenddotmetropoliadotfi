e89cb63179a03c24e27326a11682345ad341b5af
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-25 15:51:04 +0200

    Add routes for teacher worklog course management in AdminWorkLogRoutes

--- frontend/src/routes/admin/AdminWorkLogRoutes.tsx
+++ frontend/src/routes/admin/AdminWorkLogRoutes.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import AdminMainView from '../../views/main/admin/AdminMainView';
 import AdminWorkLogs from '../../views/main/admin/AdminWorkLogs';
+import TeacherWorklogCourseModify from '../../views/main/teacher/WorkLog/TeacherWorklogCourseModify';
+import TeacherWorklogCourseStats from '../../views/main/teacher/WorkLog/TeacherWorklogCourseStats';
+import TeacherWorklogCourseDetail from '../../views/main/teacher/WorkLog/TeacherWorklogCourseDetail';
+import TeacherWorklogCourseGroups from '../../views/main/teacher/WorkLog/Groups/TeacherWorklogCourseGroups';
+import TeacherWorklogCourseEntries from '../../views/main/teacher/WorkLog/TeacherWorklogCourseEntries';
+import TeacherWorklogCourseGroup from '../../views/main/teacher/WorkLog/Groups/TeacherWorklogCourseGroup';
+import TeacherWorklogCourseGroupStats from '../../views/main/teacher/WorkLog/Groups/TeacherWorklogCourseGroupStats';
+import TeacherWorklogCourseGroupEntries from '../../views/main/teacher/WorkLog/Groups/TeacherWorklogCourseGroupEntries';
 
 /**
  * AdminWorkLogRoutes component.
@@ -16,6 +24,26 @@ const AdminWorkLogRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path='/' element={<AdminWorkLogs />} />
+      <Route path=':courseid/modify' element={<TeacherWorklogCourseModify />} />
+      <Route path='stats/:courseid?' element={<TeacherWorklogCourseStats />} />
+      <Route path=':courseid' element={<TeacherWorklogCourseDetail />} />
+      <Route
+        path=':courseid/entries'
+        element={<TeacherWorklogCourseEntries />}
+      />
+      <Route path='group/:courseid' element={<TeacherWorklogCourseGroups />} />
+      <Route
+        path='group/:courseid/:groupid'
+        element={<TeacherWorklogCourseGroup />}
+      />
+      <Route
+        path='group/:courseid/:groupid/stats'
+        element={<TeacherWorklogCourseGroupStats />}
+      />
+      <Route
+        path='group/:groupid/entries'
+        element={<TeacherWorklogCourseGroupEntries />}
+      />
       <Route path='*' element={<AdminMainView />} />
     </Routes>
   );
