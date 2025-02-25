1a9dea34ea4cf74bf4caafd92e7aa278ed7dae96
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-25 17:28:32 +0200

    Add AdminServerStatus route and reorganize imports in AdminDashBoardRoutes

--- frontend/src/routes/admin/AdminDashBoardRoutes.tsx
+++ frontend/src/routes/admin/AdminDashBoardRoutes.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import AdminDashboard from '../../views/main/admin/AdminDashboard';
-import AdminErrorLogs from '../../views/main/admin/AdminErrorLogs';
+import AdminErrorLogs from '../../views/main/admin/Dashboard/AdminErrorLogs';
 import AdminFeedback from '../../views/main/admin/AdminFeedback';
-import AdminGuide from '../../views/main/admin/AdminGuide';
-import AdminLogs from '../../views/main/admin/AdminLogs';
-import AdminStats from '../../views/main/admin/AdminStats';
+import AdminGuide from '../../views/main/admin/Dashboard/AdminGuide';
+import AdminLogs from '../../views/main/admin/Dashboard/AdminLogs';
+import AdminStats from '../../views/main/admin/Dashboard/AdminStats';
+import AdminServerStatus from '../../views/main/admin/Dashboard/AdminServerStatus';
 
 /**
  * AdminDashBoardRoutes component.
@@ -15,12 +16,13 @@ import AdminStats from '../../views/main/admin/AdminStats';
 const AdminDashBoardRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<AdminDashboard />}>
+      <Route path='/' element={<AdminDashboard />}>
         <Route index element={<AdminGuide />} />
-        <Route path="stats" element={<AdminStats />} />
-        <Route path="logs" element={<AdminLogs />} />
-        <Route path="user-feedback" element={<AdminFeedback />} />
-        <Route path="errorlogs" element={<AdminErrorLogs />} />
+        <Route path='stats' element={<AdminStats />} />
+        <Route path='logs' element={<AdminLogs />} />
+        <Route path='user-feedback' element={<AdminFeedback />} />
+        <Route path='errorlogs' element={<AdminErrorLogs />} />
+        <Route path='server-status' element={<AdminServerStatus />} />{' '}
       </Route>
     </Routes>
   );
