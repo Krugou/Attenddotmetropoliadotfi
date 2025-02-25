7178e178edc75798c42c37e01f38ccda4e3d3d46
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-25 16:25:28 +0200

    Add optional buttonClassName prop to GeneralLinkButton for customizable styling

--- frontend/src/components/main/buttons/GeneralLinkButton.tsx
+++ frontend/src/components/main/buttons/GeneralLinkButton.tsx
@@ -6,15 +6,22 @@ import {useNavigate} from 'react-router-dom';
 interface GeneralLinkButtonProps {
   path: string;
   text: string;
+  buttonClassName?: string;
 }
 /**
  * A button component that navigates to a specified path when clicked.
  */
-const GeneralLinkButton: React.FC<GeneralLinkButtonProps> = ({path, text}) => {
+const GeneralLinkButton: React.FC<GeneralLinkButtonProps> = ({
+  path,
+  text,
+  buttonClassName,
+}) => {
   const navigate = useNavigate();
   return (
     <button
-      className='px-2 py-1 font-heading text-white transition rounded-sm bg-metropolia-main-orange h-fit hover:hover:bg-metropolia-secondary-orange sm:py-2 sm:px-4 focus:outline-hidden focus:shadow-outline'
+      className={`px-2 py-1 font-heading text-white transition rounded-sm bg-metropolia-main-orange h-fit hover:hover:bg-metropolia-secondary-orange sm:py-2 sm:px-4 focus:outline-hidden focus:shadow-outline ${
+        buttonClassName || ''
+      }`}
       onClick={() => navigate(path)}>
       {text}
     </button>
