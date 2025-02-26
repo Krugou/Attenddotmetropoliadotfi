81c3113a73c719c611120d074c9e7457808e9c8d
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-02-26 15:41:12 +0200

    Add AdminOtherDashboards component for role-specific dashboard navigation

---
+++ frontend/src/views/main/admin/AdminOtherDashboards.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Card from '../../../components/main/cards/Card';
+import MainViewTitle from '../../../components/main/titles/MainViewTitle';
+import {School, SupervisorAccount, People} from '@mui/icons-material';
+import {useTranslation} from 'react-i18next';
+
+/**
+ * AdminOtherDashboards component.
+ * This component displays cards for accessing different role-specific dashboards
+ * that are available to administrators.
+ *
+ * @returns {JSX.Element} The rendered AdminOtherDashboards component.
+ */
+const AdminOtherDashboards: React.FC = () => {
+  const {t} = useTranslation(['admin']);
+  return (
+    <>
+      <MainViewTitle role={'Admin'} subtitle={'Other Dashboards'} />
+      <div className='grid grid-cols-1 gap-4 p-5 ml-auto mr-auto sm:grid-cols-2 lg:grid-cols-3 w-fit'>
+        <Card
+          path='/teacher/mainview'
+          title={t('admin:mainView.teacherDashboard')}
+          description={t('admin:mainView.teacherDashboardDesc')}
+          icon={School}
+        />
+        <Card
+          path='/counselor/mainview'
+          title={t('admin:mainView.counselorDashboard')}
+          description={t('admin:mainView.counselorDashboardDesc')}
+          icon={SupervisorAccount}
+        />
+        {import.meta.env.MODE === 'development' && (
+          <Card
+            path='/student/'
+            title={t('admin:mainView.studentDashboard')}
+            description={t('admin:mainView.studentDashboardDesc')}
+            icon={People}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default AdminOtherDashboards;
