1a626a1aaabc99c993eff23cbd8cdbf0a407038f
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-04-02 21:35:35 +0300

    Remove obsolete SQL migration: deleted userid column addition from work_log_practicum table

--- backend/database/fixpracticum.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `work_log_practicum`
-ADD COLUMN `userid` INT(11) NULL AFTER `description`,
-ADD CONSTRAINT `work_log_practicum_ibfk_1`
-FOREIGN KEY (`userid`) REFERENCES `users`(`userid`) ON DELETE SET NULL;
